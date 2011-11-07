823c64ed1b0e0c46b7ab28c7b0552d2c9b75568c
emre can yılmaz <ecylmz@bil.omu.edu.tr>
2011-11-08 00:43:44 +0200

    güncellendi.

--- _scripts/gist-api.rb
+++ _scripts/gist-api.rb
@@ -76,7 +76,7 @@ def sub_page
     Dir.chdir(MAIN_PATH + label)
     gist = Hash.new
     if id_map[label].size == 1
-      gist["label"] = label
+      gist["label"] = label.first
       gist["id"] = ids
       gist["description"] = description[ids]
       gists << gist
